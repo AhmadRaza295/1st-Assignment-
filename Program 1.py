b50afe952ad5dbd5505c3b1b39cb106e894fbc28
AhmadRaza295 <152097989+AhmadRaza295@users.noreply.github.com>
2023-11-26 09:14:43 -1200

    Update Program 1.py

--- Program 1.py
+++ Program 1.py
@@ -1,3 +1,4 @@
+# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 number1 = 30  
 number2 = 40
 if (number1 * number2)<=1000:
